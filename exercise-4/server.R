acd63bb638f074a97eb3c40a4cabbbde82591ba1
Jason You <jason.you1995@gmail.com>
2016-11-15 15:26:22 -0800

    finish

--- exercise-4/server.R
+++ exercise-4/server.R
@@ -2,11 +2,11 @@
 library(dplyr)
 
 # Read in data
-setwd('~/Documents/info-201/m14-shiny/exercise-4/')
-source('./scripts/buildMap.R')
-source('./scripts/buildScatter.R')
-df <- read.csv('./data/electoral_college.csv', stringsAsFactors = FALSE)
-state.codes <- read.csv('./data/state_codes.csv', stringsAsFactors = FALSE)
+setwd("/Users/jasonyou1/Google Drive/CLASS(UW)/2016Autumn/INFO201/git201/m14-shiny/exercise-4")
+source('scripts/buildMap.R')
+source('scripts/buildScatter.R')
+df <- read.csv('data/electoral_college.csv', stringsAsFactors = FALSE)
+state.codes <- read.csv('data/state_codes.csv', stringsAsFactors = FALSE)
 
 # Join together state.codes and df
 joined.data <- left_join(df, state.codes, by="state")
@@ -20,8 +20,11 @@ shinyServer(function(input, output) {
   # Render a plotly object that returns your map
   output$map <- renderPlotly({ 
       return(BuildMap(joined.data, input$mapvar))
+    
   }) 
   
   # Create a `scatter` property on your `output` object. That property shoudl be a `renderPlotly` object that returns a scatterplot (`BuildScatter`)
-  
+  output$scatter <- renderPlotly({
+    return(BuildScatter(joined.data, input$search))
+  })
 })
